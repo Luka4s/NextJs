bce5c8aafaf77362aba84031a3a983cfb56c5f27
Lucas Rodrigues Seidel Silva <lucas.rodrigues28012003@gmail.com>
2024-09-06 15:23:59 -0300

    product/Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,27 @@
-import { styled } from "../styles";
+import { HomeContainer, Product } from "../styles/pages/home";
+import Image from "next/image";
 
-const Button = styled("button", {
-  backgroundColor: "$green300",
-  borderRadius: "5px",
-  border: 0,
-  padding: "10px 20px",
-
-  span: {
-    fontWeight: "bold",
-  },
-  "&:hover": {
-    backgroundColor: "$green500",
-  },
-});
+import camiseta1 from "./../assets/shirts/1.png";
+import camiseta2 from "./../assets/shirts/2.png";
+import camiseta3 from "./../assets/shirts/3.png";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello Wolrd</h1>
-      <Button>Teste</Button>
-    </div>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
